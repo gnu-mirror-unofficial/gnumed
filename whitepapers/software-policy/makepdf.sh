1dc01a68b983c70dc04c9d5f1a14ea6c76e370fc
hherb <hherb>
2003-03-02 13:03:08 +0000

    Creates a PDF document from the HTML formatted chapters

---
+++ whitepapers/software-policy/makepdf.sh
@@ -0,0 +1,2 @@
+html2ps -C hb -o temp.ps index.html introduction.html desirable-features.html good-tender.html licenses.html  programming-languages.html development-tools.html guitools.html conclusions.html references.html glossary.html acknowledgements.html 
+ps2pdf temp.ps software-policy.pdf
